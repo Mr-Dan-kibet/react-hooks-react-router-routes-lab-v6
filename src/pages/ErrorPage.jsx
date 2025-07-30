c989dc76a8bd158a1883df01c38c29af0433de0d
Mr-Dan-kibet <kibetdan56@gmail.com>
2025-07-30 19:46:36 +0300

    kk

---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+function ErrorPage() {
+  return (
+    <main>
+      <h1>Oops! Looks like something went wrong.</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </main>
+  );
+}
+
+export default ErrorPage;
